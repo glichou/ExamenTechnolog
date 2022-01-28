1ed303be0f8c5c81141370980ebba05023c891ae
Arzur Lichou <22329645+glichou@users.noreply.github.com>
2022-01-28 14:09:11 +0100

    Ajout méthode peuplement seeder.py

--- seed/seeder.py
+++ seed/seeder.py
@@ -17,10 +17,41 @@ class MongoSeeder:
         """
         return self.__db
 
+    def seed(self):
+        """ Peupler la base de données avec des véhicules possédant des
+        données valides. 
+        """
+        # Vider tous les véhicules dans la base de données.
+        self.db.vehicules.drop()
+
+        # Initialiser une liste de véhicules.
+        vehicules = []
+
+        # Créer un premier véhicules.
+        vehicules.append(
+            {
+                "immatriculation": "NE-212-BA 91",
+                "marque": "Cadillac",
+                "modele": "Escalade ESV",
+                "prix": 150000.00,
+                "couleur": "noire"
+            }
+        )
+        # Insérer dans la base de données les véhicules.
+        self.db.vehicules.insert_many(vehicules)
+
+    def dispayDBContent(self): 
+        """ Afficher les informations dans la base MongoDB
+        """
+        cursor = self.db.vehicules.find()
+        for vehicule in cursor:
+            print(vehicule)
+
+
 
 print("Initialisation de la base de données...")
 s = MongoSeeder("MongoDB")
 print("Peuplement de la base de données en cours...")
-
-
 s.seed()
+print("Affichage du contenu de la base...")
+s.dispayDBContent()
