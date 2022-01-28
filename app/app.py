685466a1a3da01b10ebd33e6996348af825a2e41
Arzur Lichou <22329645+glichou@users.noreply.github.com>
2022-01-28 15:34:07 +0100

    Ajout de la méthode suppression d'un vehicule

--- app/app.py
+++ app/app.py
@@ -30,7 +30,7 @@ def create_vehicule(vehicule: ModeleVehicule):
     db = database()
 
     if db.vehicules.find_one({"immatriculation": vehicule.immatriculation}) is not None:
-        raise HTTPException(status=403, detail="Ce véhicule a déjà été enregistré dans la base.")
+        raise HTTPException(status_code=403, detail="Ce véhicule a déjà été enregistré dans la base.")
     result = db.vehicules.insert_one(vehicule.dict())
     return vehicule.dict()
 
@@ -48,5 +48,11 @@ def fetch_vehicules_by_immatriculation(immatriculation: str):
             "prix": result['prix'],
             "couleur": result['couleur']
         }
-    raise HTTPException(status=404, detail="Ce véhicule n'existe pas dans la base.")
+    raise HTTPException(status_code=404, detail="Ce véhicule n'existe pas dans la base.")
     
+@app.delete("/vehicules/{immatriculation}")
+def delete_vehicule_by_immatriculation(immatriculation: str):
+    """ Supprimer un véhicule à partir de son immatriculation
+    """
+    db = database()
+    db.vehicules.delete_one({"immatriculation": immatriculation})
